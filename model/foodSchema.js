68e85adf4e6d3732a40c28c81841e8876c657444
lucky-panchal <luckykanti31122006@gmail.com>
2024-12-21 10:46:07 +0530

    Prepared by and rating schema added

--- model/foodSchema.js
+++ model/foodSchema.js
@@ -4,35 +4,49 @@ const foodSchema = mongoose.Schema({
         Starter :{
             Springroll :{
                 name : String,
-                price : Number
+                price : Number,
+                rating: String,
+                PreparedBy: String
                 },
                 Vegitarian:{
                     name : String,
-                    price : Number
+                    price : Number,
+                    rating: String,
+                    PreparedBy: String
                     },
                 Nonvegitarian :{
                     name : String,
-                    price : Number
+                    price : Number,
+                    rating: String,
+                    PreparedBy: String
                     }
                 }, 
         Mainfood:{
             PunjabiThali:{
                 name : String,
-                price : Number                
+                price : Number,
+                rating: String,
+                PreparedBy: String                
             },
             ChickenKabab:{
                 name : String,
-                price : Number
+                price : Number,
+                rating: String,
+                PreparedBy: String
                 },
             },
         Desert:{
             GulabJumun:{
                 name : String,
-                price : Number
+                price : Number,
+                rating: String,
+                PreparedBy: String
                 },
             Waffle:{
                 name : String,
-                price : Number
+                price : Number,
+                rating: String,
+                PreparedBy: String
                 },
             }    
                 }
