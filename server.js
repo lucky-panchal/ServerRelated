6c07304b6fa789fd0196709028b86a27329def86
lucky-panchal <luckykanti31122006@gmail.com>
2025-01-06 14:34:04 +0530

    Update Api

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require ('express');
 const app= express();
-const Model=require ('./model/userSchema')
 const PORT = 3030;
 const connectDB= require('./config/db');
 const UserModel = require('./model/userSchema');
@@ -13,19 +12,32 @@ app.get('/', (req, res) => {
 app.post("/register", async (req, res) => {
 try{
     const {email,name,password} = req.body
-    const userExist = await UserModel.findOne({password})
+    const userExist = await UserModel.findOne({name})
     if(userExist){
      return res.send({message: "User Existed"})
     }
-    const user = await UserModel.create(req.body)
+    const user = await UserModel.create(req.body.name)
     UserModel.save();
-    res.send({message: "User Created Successfully"})
+    return res.send({message: "User Created Successfully"})
 
 }
 catch(err){
     return res.send(err)
 }
-    
+    try{
+        app.put("/update/id" , async (req,res)=>{
+        const id =req.params.id;
+        const{name,email,password} =req.body
+        const userExist = await UserModel.findOneAndUpdate({name})
+        if(userExist){
+     return res.send({message: "Updated"})
+    }else{
+        res.send({message:"User Not Exist" })
+    }
+        })
+    }catch(err){
+        res.send(err)
+    }
 })
     app.listen(PORT, () => {
       console.log("Server is running successfully");
