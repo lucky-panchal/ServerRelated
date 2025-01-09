7385298c55ad4fd95145d19c4a4cae315f15cbba
lucky-panchal <luckykanti31122006@gmail.com>
2025-01-09 11:36:08 +0530

    delete and update api

--- server.js
+++ server.js
@@ -9,7 +9,10 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
  res.send("hello , lux here...")
 })
-app.post("/register", async (req, res) => {
+
+
+
+app.post('/register', async (req, res) => {
 try{
     const {email,name,password} = req.body
     const userExist = await UserModel.findOne({name})
@@ -22,22 +25,37 @@ try{
 }
 catch(err){
     return res.send(err)
-}
-    try{
-        app.put("/update/:id" , async (req,res)=>{
-        const id =req.params.id;
-        const{name,email,password} =req.body
-        const userExist = await UserModel.findOneAndUpdate({name})
-        if(userExist){
-     return res.send({message: "Updated"})
-    }else{
-        res.send({message:"User Not Exist" })
-    }
-        })
-    }catch(err){
-        res.send(err)
+}    
+})
+app.delete('/delete/:_id',   async (req, res) => {
+    const id = req.params._id;
+    const userDelete = await UserModel.findOneAndDelete(id)
+    if (userDelete) {
+        res.send({ message: "User Deleted Successfully" })
+    } else {
+        res.send({ message: "User not exist" })
     }
 })
+
+app.put('/update/:id', async (req, res) => {
+    const itemId = req.params.id
+    const updateId = req.body
+    console.log(updateId)
+    const userUpdate = await UserModel.findByIdAndUpdate({ _id: itemId }, updateId, { name: true })
+    try {
+        if (userUpdate) {
+            res.send({ message: "User Updated Successfully" })
+        }
+        else {
+            res.send({ message: "User not updated Successfully" })
+        }
+    }
+    catch {
+        res.send("error");
+
+    }
+})
+
     app.listen(PORT, () => {
       console.log("Server is running successfully");
     })
